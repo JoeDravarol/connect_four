65c72117f3830af58cf53df7f729b70f7b339d96
Joe <joedravarol@gmail.com>
2019-05-25 02:43:56 +0100

    Add Board Test & Code

---
+++ lib/board.rb
@@ -0,0 +1,45 @@
+class Board
+  attr_reader :square
+
+  def initialize
+    @square = []
+    # Create multi-dimensional array for columns and rows
+    # Outer array is columns and sub array is rows
+    7.times { square << Array.new(6) { " " }}
+  end
+
+  def display
+    line = ""
+    7.times { line << "----" }
+    puts line
+
+    row = 0
+
+    while row < 6
+      row_square = "|"
+      7.times { |col| row_square << " #{square[col][row]} |" }
+
+      puts row_square
+      puts line
+
+      row += 1
+    end
+    puts "  0   1   2  3   4   5   6"
+  end
+
+  def drop_mark(col, mark)
+    return "Invalid drop" if !col.between?(0,6) || square[col][0] != " " 
+
+    row = square[col].length - 1
+      
+    until row < 0
+      if square[col][row] == " "
+        square[col][row] = mark
+        break
+      end
+
+      row -= 1
+    end
+    display
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,45 @@
+require 'board'
+
+RSpec.describe Board do
+  let(:board) do
+    @board = Board.new
+  end
+
+  describe "#initialize" do
+    it "creates a 7 column square" do
+      expect(board.square.length).to eql(7)
+    end
+
+    it "creates a 6 row squares" do
+      board.square.each do |row|
+        expect(row.length).to eql(6)
+      end
+    end
+  end
+
+  describe "#drop_mark" do
+    it "drops mark on the bottom of empty column" do
+      board.drop_mark(0, "X")
+      expect(board.square[0][5]).to eql("X")
+    end
+
+    it "drops mark on top of another mark" do
+      board.drop_mark(0, "X")
+      board.drop_mark(0, "O")
+      expect(board.square[0][4]).to eql("O")
+    end
+
+    context "returns error message" do
+      it "when drop mark on a column that is full" do
+        # Fill all the columns
+        6.times { board.drop_mark(2, "X") }
+  
+        expect(board.drop_mark(2, "O")).to eql "Invalid drop"
+      end
+
+      it "when drop mark on non-existing column" do
+        expect(board.drop_mark(7, "X")).to eql "Invalid drop"
+      end
+    end
+  end
+end
