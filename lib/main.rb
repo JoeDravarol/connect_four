af3aa079dccd7d280e8457df6db619ad80f538d6
Joe <joedravarol@gmail.com>
2019-05-26 22:38:05 +0100

    Connect all the modules into main.rb & added tests

---
+++ lib/main.rb
@@ -0,0 +1,107 @@
+require './lib/board'
+require './lib/player'
+require './lib/win_condition'
+
+class Game
+  attr_accessor :board, :player_1, :player_2, :current_player, :win_condition
+  # Only use for testing
+  attr_reader :name_1, :name_2
+
+  def initialize
+    @board = Board.new
+    @win_condition = WinCondition.new
+
+    @name_1 = get_name("player 1")
+    @player_1 = Player.new(name_1, "\u2600")
+
+    @name_2 = get_name("player 2")
+    @player_2 = Player.new(name_2, "\u263C")
+    @current_player = player_1
+
+    play_game
+  end
+
+  def get_name(player)
+    puts "Hi #{player}, what is your name?"
+    name = gets.chomp.capitalize
+    puts "Nice to meet you #{name}"
+    name
+  end
+
+  def display_instructions
+    puts "***************************************"
+    puts "** Welcome To The Connect Four Game! **"
+    puts "***************************************"
+    puts "======================================="
+    puts "***************************************"
+    puts "************ Instructions *************"
+    puts "***************************************"
+    puts "======================================="
+    puts "1. The goal of this game is to get four"
+    puts "of your mark in line up horizontally, vertically, or diagonally."
+    puts "2. The players takes turn to drop their mark"
+    puts "in  a column by entering the column number (shown below the board)."
+    puts "3. If the board is filled, with no victors, it will be a draw."
+    puts "#{player_1.name}, your mark is #{player_1.mark}"
+    puts "#{player_2.name}, your mark is #{player_2.mark}"
+    puts "Good luck and have fun!"
+  end
+
+  def get_mark(player)
+    puts "It's your turn #{player.name}"
+    puts "Select a column to drop your mark"
+
+    col = gets.chomp
+
+    until col_valid?(col)
+      "Please enter a valid input"
+      col = gets.chomp
+    end
+
+    col.to_i
+  end
+
+  def col_valid?(col)
+    # Because the regex returns nil when it's not match, the ternary operator makes it so that it returns false
+    !col.empty? && col.length == 1 && col =~ (/\d+/) && col.to_i.between?(0,6) ? true : false
+  end
+
+  def drop_mark(player)
+    col = get_mark(player)
+    player.drop_mark(col, board)
+  end
+
+  def switch_player
+    @current_player = current_player == player_1 ? player_2 : player_1
+  end
+
+  def play_game
+    display_instructions
+    board.display
+
+    loop do
+      col = drop_mark(current_player)
+      break if game_ended?(current_player, board)
+      switch_player
+    end
+  end
+
+  def game_ended?(player, board)
+    if won?(player, board)
+      puts "Game Over! #{player.name} has won!"
+      return true
+    elsif draw?(board)
+      puts "Game Over! It's a draw"
+      return true
+    end
+    false
+  end
+
+  def won?(player, board)
+    win_condition.win?(board.current_drop_position, player.mark, board)
+  end
+
+  def draw?(board)
+    win_condition.draw?(board)
+  end
+end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,73 @@
-require 'main.rb'
+require 'main'
+
+RSpec.describe Game do
+  let(:game) { @game = Game.new }
+
+  describe "#initialize" do
+    context "Create a board" do
+      it "creates a 7 column square" do
+        expect(game.board.square.length).to eql(7)
+      end
+  
+      it "creates a 6 row squares" do
+        game.board.square.each do |row|
+          expect(row.length).to eql(6)
+        end
+      end
+    end
+
+    it "get player 1 name" do
+      expect(game.player_1.name).to eql game.name_1
+    end
+
+    it "get player 2 name" do
+      expect(game.player_2.name).to eql game.name_2
+    end
+  end
+
+  describe "#col_valid?" do
+    it "returns true when column entered is between 0 to 6" do
+      6.times do |col|
+        expect(game.col_valid?("#{col}")).to eql(true)
+      end
+    end
+
+    context "returns false" do
+      it "when given emtpy input" do
+        expect(game.col_valid?("")).to eql(false)
+      end
+
+      it "when input is a space" do
+        expect(game.col_valid?(" ")).to eql(false)
+      end
+
+      it "when a character is entered" do
+        expect(game.col_valid?("X")).to eql(false)
+      end
+
+      it "when a string is entered" do
+        expect(game.col_valid?("space")).to eql(false)
+      end
+
+      it "when the column entered in not between 0 to 6" do
+        expect(game.col_valid?("20")).to eql(false)
+      end
+    end
+  end
+
+  describe "#switch_player" do
+    it "switch from player 1 to player 2" do
+      game.current_player = game.player_1
+      game.switch_player
+
+      expect(game.current_player).to eql(game.player_2)
+    end
+
+    it "switch from player 2 to player 1" do
+      game.current_player = game.player_2
+      game.switch_player
+
+      expect(game.current_player).to eql(game.player_1)
+    end
+  end
+end
